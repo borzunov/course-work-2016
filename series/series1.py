642a4ef31f81e2e1a420799356df76ab827852b4
Alexander Borzunov <hxrussia@gmail.com>
2016-06-24 23:33:17 +0500

    Clean series and bruteforce output

--- series/series1.py
+++ series/series1.py
@@ -3,10 +3,9 @@ import sys
 n = int(sys.argv[1])  # n >= 2
 
 result = []
-for i in range(n + 1, 1, -1):  # For i = n + 1 .. 2 (inclusive)
+for i in range(n, 0, -1):
     result.append('10' * i + '0')
-result.append('10')
 print(''.join(result))
 
-# n = 2 * k, L(S_n) = 2 * k + 3, Z(S_n) = k + 3
+# n = 2 * k, L(S_n) = 2 * k + 2, Z(S_n) = k + 3
 # L(S_n) / Z(S_n) -> 2
--- series/series2.py
+++ series/series2.py
@@ -3,10 +3,10 @@ import sys
 n = int(sys.argv[1])  # n >= 2
 
 result = []
-for k in range(n + 1, 1, -1):
-    for i in range(k + 1, 1, -1):
+for k in range(n, 0, -1):
+    for i in range(k, 0, -1):
         result.append('10' * i + '0')
 print(''.join(result))
 
-# n = 2 * k, L(S_n) = 4 * k + 2, Z(S_n) = 3 * k + 2
+# n = 2 * k, L(S_n) = 4 * k, Z(S_n) = 3 * k
 # L(S_n) / Z(S_n) -> 4/3
